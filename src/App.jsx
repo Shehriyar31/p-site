4e5296a67827f3bee8870f25e15c975b24bba11a
Shehriyar31 <muhammadshehriyar31@gmail.com>
2025-08-14 10:43:56 +0500

    Add complete login/signup system with preloader and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import LoginForm from './components/LoginForm'
 import SignUpForm from './components/SignUpForm'
 import ErrorPage from './components/ErrorPage'
+import Preloader from './components/Preloader'
 import './components/LoginForm.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return <Preloader />
+  }
+
   return (
     <Router>
       <Routes>
